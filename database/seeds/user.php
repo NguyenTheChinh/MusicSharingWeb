4d8d8ad8bac9f892af8bddd6cb622645a9535167
NguyenTheChinh <chinh2597@gmail.com>
2018-04-17 08:29:50 +0700

    fix user seed

--- database/seeds/user.php
+++ database/seeds/user.php
@@ -24,8 +24,14 @@ class user extends Seeder
     public function run()
     {
         DB::table('user')-> insert(
-            ['id'=>4,'username'=>'An', 'password'=>'Anbk', 'full_name'=>'Vũ Đình An', 'birthday'=>'1997-05-24', 'email'=>'Vinhdzbk@gmail.com', 'phone'=>'0917339848',
-                'level'=>0]  );
+            ['id'=>1,'username'=>'Dong', 'password'=>bcrypt('12345678'), 'full_name'=>'Vũ Đức Đông', 'birthday'=>'1997-11-07', 'email'=>'vudong71197@gmail.com', 'phone'=>'01235533984'],
+
+            ['id'=>2,'username'=>'Tien', 'password'=>bcrypt('123456'), 'full_name'=>'Phạm Đức Tiến', 'birthday'=>'1997-10-05', 'email'=>'Tiendzbk@gmail.com', 'phone'=>'01236665880'] ,
+
+            ['id'=>3,'username'=>'Vinh', 'password'=>bcrypt('Vinh1997'), 'full_name'=>'Trần Văn Vinh', 'birthday'=>'1997-10-05', 'email'=>'Vinhdzbk@gmail.com', 'phone'=>'0917339848'],
+
+            ['id'=>4,'username'=>'Chinh', 'password'=>bcrypt('123456'), 'full_name'=>'Nguyen The Chinh', 'birthday'=>'1997-05-20', 'email'=>'Chinhdzbk@gmail.com', 'phone'=>'0917339848']
+        );
     }
 
 
