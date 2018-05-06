40e5464088af01064a5bd15f1b4e3590692ae55f
hieuiabc <hieuiabc@gmail.com>
2018-05-06 15:17:24 +0700

    cap nhat playlist

--- app/Playlist.php
+++ app/Playlist.php
@@ -8,7 +8,7 @@ class Playlist extends Model
 {
     protected $table = "playlist";
     protected $fillable = ['name', 'user_id'];
-    public $timestamps = false;
+    public $timestamps = true;
 
     public function ListTrack()
     {
